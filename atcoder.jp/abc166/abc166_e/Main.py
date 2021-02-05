c03f1b4f70db8851800237cface8a36fbc526223
mono_0812 <frisk02.jar@gmail.com>
2021-02-05 16:46:34 +0900

    [AC] abc166 abc166_e

---
+++ atcoder.jp/abc166/abc166_e/Main.py
@@ -0,0 +1,10 @@
+from collections import Counter
+n=int(input())
+A=list(map(int,input().split()))
+P= [i+a for i,a in enumerate(A,start=1)]
+Q=[j-a for j,a in enumerate(A,start=1)]
+ans=0
+QC = Counter(Q)
+for p in P:
+    ans+=QC[p]
+print(ans)
