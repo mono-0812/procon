0bd70fafa1adf1e696ffa8d72a1a435226cd41fc
unknown <frisk02.jar@gmail.com>
2021-04-07 21:14:56 +0900

    atcoder

---
+++ atcoder.jp/hitachi2020/hitachi2020_a/Main.py
@@ -0,0 +1,26 @@
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int, input().split())
+def LIIS(): return list(map(int, input().split()))
+def ZER(N): return [False for _ in range(N)]
+
+
+INF = 10**30
+MOD = 10**9+7
+
+
+#         V
+#    ／￣ψ￣＼
+#   | 合格祈願 |
+#   |＿＿＿＿＿|
+
+
+##############################################################################
+
+
+s=I()
+if s.count("hi")*2==len(s):
+    print("Yes")
+else:
+    print("No")
