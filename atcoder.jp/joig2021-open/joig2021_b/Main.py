a708b325418622cfb3b16c1558c49065cdc95b3e
unknown <frisk02.jar@gmail.com>
2021-04-29 21:15:00 +0900

    atcoder

---
+++ atcoder.jp/joig2021-open/joig2021_b/Main.py
@@ -0,0 +1,26 @@
+import bisect,collections,copy,heapq,itertools,math,string,sys,queue
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int,input().split())
+def LIIS(): return list(map(int,input().split()))
+def ZER(N): return [False for _ in range(N)]
+INF=float("inf")
+MOD=10**9+7
+def make_divisors(n):
+    lower_divisors , upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+##############################################################################
+n,k=IIS()
+t=I()
+ans1=t[:k-1]
+ans2=t[k-1:]
+
+print(ans1+ans2.swapcase())
---
+++ atcoder.jp/joig2021-open/joig2021_c/Main.py
@@ -0,0 +1,36 @@
+import bisect,collections,copy,heapq,itertools,math,string,sys,queue
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int,input().split())
+def LIIS(): return list(map(int,input().split()))
+def ZER(N): return [False for _ in range(N)]
+INF=float("inf")
+MOD=10**9+7
+def make_divisors(n):
+    lower_divisors , upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+##############################################################################
+n=II()
+A=LIIS()
+on=0
+off=0
+r=0
+val=0
+ans=0
+for i in range(n):
+    if A[i]:on+=1
+    else:off+=1
+    if on-off>=val:
+        r=i
+        val=on-off
+for i in A:
+    if i:ans+=1
+print(ans-val)
