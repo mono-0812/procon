a70a6b199e5378744425c0f1db500396fb1cecae
mono-0812 <frisk02.jar@gmail.com>
2021-06-21 21:15:07 +0900

    atcoder

---
+++ atcoder.jp/abc206/abc206_a/Main.py
@@ -0,0 +1,36 @@
+import bisect,collections,copy,heapq,itertools,math,string,sys,queue,time
+input = lambda: sys.stdin.readline().rstrip()
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int,input().split())
+def LIIS(): return list(map(int,input().split()))
+def debug(*args): print(*args) if len(args)>0 else print(False);return
+start_time=time.time()
+def nt(): print(time.time()-start_time);return
+def chmax(a: list, b: list) -> bool:
+     assert len(a) == len(b) == 1, "1要素のlistを指定してください"
+     if a[0] < b[0]:
+         a[0] = b[0]
+         return True
+     return False
+def chmin(a: list, b: list) -> bool:
+    assert len(a) == len(b) == 1, "1要素のlistを指定してください"
+    if a[0] > b[0]:
+        a[0] = b[0]
+        return True
+    return False
+def combinations_count(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+ragen=range
+INF=10**20
+MOD=10**9+7
+from decimal import Decimal as d
+##############################################################################
+n=II()
+if math.floor(1.08*n)<206:
+    print("Yay!")
+elif math.floor(1.08*n)==206:
+    print("so-so")
+else:
+    print(":(")
---
+++ atcoder.jp/abc206/abc206_d/Main.py
@@ -0,0 +1,96 @@
+import bisect,collections,copy,heapq,itertools,math,string,sys,queue,time
+from typing import Union
+input = lambda: sys.stdin.readline().rstrip()
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int,input().split())
+def LIIS(): return list(map(int,input().split()))
+def debug(*args): print(*args) if len(args)>0 else print(False);return
+start_time=time.time()
+def nt(): print(time.time()-start_time);return
+def chmax(a: list, b: list) -> bool:
+     assert len(a) == len(b) == 1, "1要素のlistを指定してください"
+     if a[0] < b[0]:
+         a[0] = b[0]
+         return True
+     return False
+def chmin(a: list, b: list) -> bool:
+    assert len(a) == len(b) == 1, "1要素のlistを指定してください"
+    if a[0] > b[0]:
+        a[0] = b[0]
+        return True
+    return False
+def combinations_count(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+ragen=range
+INF=10**20
+MOD=10**9+7
+from decimal import Decimal as d, DefaultContext
+##############################################################################
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+n=II()
+A=LIIS()
+uf=UnionFind(2*10**5+10)
+li1=A[:n//2]
+li2=A[math.ceil(n/2):][::-1]
+val2=0
+for i in range(n//2):
+    uf.union(li1[i],li2[i])
+    if li1[i]==li2[i]:
+        val2+=1
+li=uf.all_group_members()
+val=0
+for i in li:
+    val+=1
+print(200010-val)
